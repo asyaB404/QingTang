08c3e4bb2130611100b0ccc292f0aaaf728e56c2
bbbasya <42473270+asyaB404@users.noreply.github.com>
2025-03-14 11:13:41 +0800

    no message

--- Assets/Scripts/UI/Panel/DialogPanel.cs
+++ Assets/Scripts/UI/Panel/DialogPanel.cs
@@ -11,22 +11,44 @@ using Data;
 using DG.Tweening;
 using GamePlay;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace UI.Panel
 {
     public class DialogPanel : BasePanel<DialogPanel>
     {
-        [SerializeField]private DialogManager dialogManager;
+        [SerializeField] private DialogManager dialogManager;
+        [SerializeField] private RectTransform tipPanelTransform;
+
         public override void Init()
         {
             base.Init();
             dialogManager.Init();
+            GetControl<Button>("tipBtn").onClick.AddListener(ChangeTipPanel);
+            GetControl<Button>("exit").onClick.AddListener(ChangeTipPanel);
         }
 
         public void SetBackGround(int sceneId)
         {
             GetControl<Image>("BG").sprite = GlobalConfig.Instance.GetBackGround(sceneId);
+        }
+
+        public void ChangeTipPanel()
+        {
+            GetControl<Button>("tipBtn").transform.GetChild(0).gameObject.SetActive(false);
+            if (tipPanelTransform.anchoredPosition.x > 0)
+            {
+                tipPanelTransform.DOAnchorPosX(0, UIConst.UI_PANEL_ANIM);
+            }
+            else
+            {
+                tipPanelTransform.DOAnchorPosX(tipPanelTransform.sizeDelta.x, UIConst.UI_PANEL_ANIM);
+            }
+        }
+
+        public void OnUpdateTip()
+        {
         }
 
         public override void OnPressedEsc()
@@ -43,10 +65,7 @@ namespace UI.Panel
         public override void HideAnim()
         {
             CanvasGroupInstance.interactable = false;
-            CanvasGroupInstance.DOFade(0f, UIConst.UI_PANEL_ANIM).OnComplete(() =>
-            {
-                gameObject.SetActive(false);
-            });
+            CanvasGroupInstance.DOFade(0f, UIConst.UI_PANEL_ANIM).OnComplete(() => { gameObject.SetActive(false); });
         }
     }
 }
