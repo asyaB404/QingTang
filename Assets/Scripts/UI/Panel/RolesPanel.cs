77f2961e4767279d718e94e81486d1d1942264bf
bbbasya <42473270+asyaB404@users.noreply.github.com>
2025-03-16 21:04:18 +0800

    .。。。

--- Assets/Scripts/UI/Panel/RolesPanel.cs
+++ Assets/Scripts/UI/Panel/RolesPanel.cs
@@ -7,9 +7,17 @@
 // //   (___)___)                         @Copyright  Copyright (c) 2025, Basya
 // // ********************************************************************************************
 
+using UnityEngine.UI;
+
 namespace UI.Panel
 {
     public class RolesPanel : BasePanel<RolesPanel>
     {
+        public override void Init()
+        {
+            base.Init();
+            UIManager.Instance.AddExcludedPanels(GetType());
+            GetControl<Button>("exit").onClick.AddListener(HideMe);
+        }
     }
 }
