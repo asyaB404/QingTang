d972bc3398901090455e383282ee58bb10424625
bbbasya <42473270+asyaB404@users.noreply.github.com>
2025-03-17 21:27:18 +0800

    10%

--- Assets/Scripts/UI/Panel/AchievePanel.cs
+++ Assets/Scripts/UI/Panel/AchievePanel.cs
@@ -7,9 +7,47 @@
 // //   (___)___)                         @Copyright  Copyright (c) 2025, Basya
 // // ********************************************************************************************
 
+using DG.Tweening;
+using GamePlay;
+using UnityEngine.UI;
+
 namespace UI.Panel
 {
     public class AchievePanel : BasePanel<AchievePanel>
     {
+        public override void Init()
+        {
+            base.Init();
+            UIManager.Instance.AddExcludedPanels(GetType());
+            GetControl<Button>("exit").onClick.AddListener(HideMe);
+            GetControl<Button>("01").onClick.AddListener(() =>
+            {
+                GetControl<Button>("01").transform.GetChild(0).gameObject.SetActive(false);
+                if (SaveManager.Instance.CheckHasFinishedDialog(8)) return;
+                DialogManager.Instance.Load(8);
+                TipPanel.Instance.ShowTip("老王好感度降低，老李好感升高。居民声望增高，指标达成+1");
+            });
+            MyEventSystem.Instance.AddEventListener<int>(CMDNAME.EVENT, (eId) =>
+            {
+                if (eId == 71)
+                {
+                    GetControl<Button>("01").gameObject.SetActive(true);
+                }
+            });
+        }
+        public override void ShowAnim()
+        {
+            CanvasGroupInstance.DOKill(true);
+            gameObject.SetActive(true);
+            CanvasGroupInstance.interactable = true;
+            CanvasGroupInstance.DOFade(1f, UIConst.UI_PANEL_ANIM);
+        }
+
+        public override void HideAnim()
+        {
+            CanvasGroupInstance.DOKill(true);
+            CanvasGroupInstance.interactable = false;
+            CanvasGroupInstance.DOFade(0f, UIConst.UI_PANEL_ANIM).OnComplete(() => { gameObject.SetActive(false); });
+        }
     }
 }
---
+++ Assets/Scripts/UI/Panel/EndPanel.cs
@@ -0,0 +1,56 @@
+// // ********************************************************************************************
+// //     /\_/\                           @file       EndPanel.cs
+// //    ( o.o )                          @brief     QingTang
+// //     > ^ <                           @author     Basya
+// //    /     \
+// //   (       )                         @Modified   2025031721
+// //   (___)___)                         @Copyright  Copyright (c) 2025, Basya
+// // ********************************************************************************************
+
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Panel
+{
+    public class EndPanel:BasePanel<EndPanel>
+    {
+        public override void Init()
+        {
+            base.Init();
+            UIManager.Instance.AddExcludedPanels(GetType());
+            GetControl<Button>("Main").onClick.AddListener(End);
+            
+        }
+
+        private void End()
+        {
+            GetControl<Image>("End").DOFade(1f, UIConst.UI_PANEL_ANIM * 10f).OnComplete(() =>
+            {
+                GetControl<Image>("End").color = Color.clear;
+                UIManager.Instance.ClearPanels();
+                GameStartPanel.Instance.ShowMe();
+            });
+        }
+
+        public override void ShowAnim()
+        {
+            CanvasGroupInstance.DOKill(true);
+            gameObject.SetActive(true);
+            CanvasGroupInstance.interactable = true;
+            CanvasGroupInstance.DOFade(1f, UIConst.UI_PANEL_ANIM);
+        }
+
+        public override void HideAnim()
+        {
+            CanvasGroupInstance.DOKill(true);
+            CanvasGroupInstance.interactable = false;
+            CanvasGroupInstance.DOFade(0f, UIConst.UI_PANEL_ANIM).OnComplete(() => { gameObject.SetActive(false); });
+        }
+
+        public override void OnPressedEsc()
+        {
+            
+        }
+    }
+}
